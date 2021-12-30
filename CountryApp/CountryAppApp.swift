0387cae7b9fea027bcc80a4dcec289a257f1b182
Giovanni Romaniello <giovanni.romaniello@vodafone.com>
2021-12-30 15:52:45 +0100

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Giovanni Romaniello on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
